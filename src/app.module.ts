ddc4dd52b5bcbd721babff81a3e8f7a5ecdcdc21
Vishal Kaushik <vishalkaushik@Vishals-MacBook-Pro.local>
2021-11-11 21:30:20 +0530

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [
+    CatsModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
+    MongooseModule.forRoot(
+      'mongodb+srv://Vishal_Kaushik:gIMYMDAMthVpeXWR@cluster0.e81sb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
